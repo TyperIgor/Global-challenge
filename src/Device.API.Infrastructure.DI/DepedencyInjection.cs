e0d4ae3c467636b03f30cc26ff2fd1bae37786a5
Igor lima <igor_purosso@hotmail.com>
2026-01-09 12:11:34 -0300

    add 'todo' comments for future improvements

--- src/Device.API.Infrastructure.DI/DepedencyInjection.cs
+++ src/Device.API.Infrastructure.DI/DepedencyInjection.cs
@@ -24,10 +24,10 @@ namespace Device.API.Infrastructure.DI
             #region Infrastructure db context
             services.AddDbContext<AppDbContext>(options =>
                     options.UseNpgsql(
-                    configuration.GetConnectionString("Postgres"),
-                    b => b.MigrationsAssembly("Device.API.Infrastructure.Data")));
+                    configuration.GetConnectionString("Postgres"), // ToDO : Move connection string to secret manager AWS secret or Azure Key vault whaetever
+                    b => b.MigrationsAssembly("Device.API.Infrastructure.Data"))); //Configure EF Migrations Assembly 
 
-            services.AddScoped<IDbContext, DBContext>();
+            services.AddScoped<IDbContext, DBContext>(); //Manual db context to handle npgsql connections
             services.AddScoped<IDeviceRepository, DeviceRepositoryImp>();
             #endregion
 
